3281600821f86c62057968d3c4d542f9854698c0
Aumy <aumy@outlook.jp>
2022-02-14 14:31:34 +0900

    nth~palindrome

--- lib/lib.ml
+++ lib/lib.ml
@@ -2,3 +2,20 @@ let rec last = function [] -> None | [ l ] -> Some l | _ :: t -> last t
 
 let rec last_two list =
   match list with [] -> None | [ a; b ] -> Some (a, b) | _ :: t -> last_two t
+
+let rec nth list n =
+  match list with
+  | [] -> None
+  | h :: t -> if n = 0 then Some h else nth t (n - 1)
+
+let length list =
+  let rec inner n = function [] -> n | _ :: t -> inner (n + 1) t in
+  inner 0 list
+
+let reverse list =
+  let rec inner list acc =
+    match list with [] -> acc | h :: t -> inner t (h :: acc)
+  in
+  inner list []
+
+let is_palindrome list = list = reverse list
--- test/test.ml
+++ test/test.ml
@@ -21,6 +21,40 @@ let last_two =
     test_case "returns None for the list with single element" `Quick case_1;
   ]
 
+let nth =
+  let check = check (option string) in
+  let case_0 () = check "" (Some "c") (nth [ "a"; "b"; "c"; "d"; "e" ] 2) in
+  let case_1 () = check "" None (nth [ "a" ] 2) in
+  [
+    test_case "returns 2nd element of the list" `Quick case_0;
+    test_case "returns None when out of index" `Quick case_1;
+  ]
+
+let length =
+  let check = check int in
+  let case_0 () = check "" 3 (length [ "a"; "b"; "c" ]) in
+  let case_1 () = check "" 0 (length []) in
+  [
+    test_case "returns 3 for the list which has 3 elements" `Quick case_0;
+    test_case "returns 0 for the list which has no elements" `Quick case_1;
+  ]
+
+let reverse =
+  let check = check (list string) in
+  let case () = check "" [ "c"; "b"; "a" ] (reverse [ "a"; "b"; "c" ]) in
+  [ test_case "returns reversed list" `Quick case ]
+
+let palindrome =
+  let check = check bool in
+  let case_0 () =
+    check "booa" true (is_palindrome [ "x"; "a"; "m"; "a"; "x" ])
+  in
+  let case_1 () = check "" false (is_palindrome [ "a"; "b" ]) in
+  [
+    test_case "returns true for palindrome list" `Quick case_0;
+    test_case "returns false for non-palindrome list" `Quick case_1;
+  ]
+
 let () =
   Alcotest.run "Example.Lib"
     [
@@ -30,4 +64,8 @@ let () =
           test_case "returns None for empty list" `Quick tail_of_a_list_2;
         ] );
       ("last_two", last_two);
+      ("nth", nth);
+      ("length", length);
+      ("reverse", reverse);
+      ("palindrome", palindrome);
     ]
